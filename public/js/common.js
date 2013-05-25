de942168cbf287a5a9e22193672eed9c12cf058a
soomtong <soomtong@gmail.com>
2013-05-25 22:23:25 +0900

    `update` remove ajax

--- public/js/common.js
+++ public/js/common.js
@@ -1,24 +1,3 @@
-var bindKey = function (menuData) {
-    var idx, keyBind;
-
-    // bind home key
-    jwerty.key('`', function () {
-        window.location.href = '/';
-    });
-
-    // bind menu key
-    for (idx in menuData) {
-        if (menuData.hasOwnProperty(idx)) {
-            keyBind = menuData[idx];
-            (function (menu) {
-                jwerty.key(menu.key, function () {
-                    window.location.href = menu.link;
-                });
-            })(keyBind);
-        }
-    }
-};
-
 $(document).ajaxStart(function () {
     $('#loading').spin();
 }).ajaxStop(function () {
