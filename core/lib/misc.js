aaaaacc6a1ca8f6a6e44757824ae18c517ba2428
soomtong <soomtong@gmail.com>
2016-07-19 09:24:37 +0900

    added router default json file

--- core/lib/misc.js
+++ core/lib/misc.js
@@ -1,41 +1,7 @@
 var fs = require('fs');
 var winston = require('winston');
 
-var defaultRoute = {
-    "root": "/",
-    "about": "/about",
-    "admin_root": "/admin",
-    "admin": {
-        "login": "/login",
-        "install": "/install",
-        "database_setup": "/make-db-config",
-        "database_init": "/make-db-init",
-        "theme_setup": "/theme-config"
-    },
-    "account_root": "/account",
-    "account": {
-        "signIn": "/sign-in",
-        "signUp": "/sign-up",
-        "signOut": "/sign-out",
-        "login": "/login",
-        "register": "/register",
-        "info": "/info",
-        "updateInfo": "/update"
-    },
-    "guestbook_root": "/guestbook",
-    "guestbook": {
-        "form": "/",
-        "message": "/register",
-        "reply": "/register/reply"
-    },
-    "teamblog_root": "/blog",
-    "teamblog": {
-        "list": "/",
-        "write": "/write",
-        "view": "/view"
-    },
-    "pages": []
-};
+var defaultRoute = require('../config/route_default.json');
 
 function setRouteTable() {
     BLITITOR.route = defaultRoute;
@@ -43,11 +9,7 @@ function setRouteTable() {
     return BLITITOR.route;
 }
 
-function databaseTablePrefix() {
-    return common.databaseDefault.prefix;
-}
-
-/*
+// deprecated
 function setRoutePage() {
     var theme = BLITITOR.config.site.theme;
     BLITITOR.route = defaultRoute;
@@ -69,7 +31,6 @@ function setRoutePage() {
         winston.error('Default theme is not exist');
     }
 }
-*/
 
 function siteThemeType() {
     return {
@@ -83,24 +44,11 @@ function showRouteTable(routeTable) {
     winston.info("\x1B[32m=== load default route table ===\033[0m");
 
     winston.verbose(JSON.stringify(routeTable, null, 4));
-/*
-    var margin = 30;
-
-    for (var item in routeTable) {
-        if (routeTable.hasOwnProperty(item)) {      // for lint
-            var pad = (new Array(margin - item.toString().length)).join(' ');
-            var tempString = item.toString() + pad;
-
-            console.log(' - ' + tempString + routeTable[item]);
-        }
-    }
-*/
 }
 
 module.exports = {
     setRouteTable: setRouteTable,
-    databaseTablePrefix: databaseTablePrefix,
-    // setRoutePage: setRoutePage,
+    setRoutePage: setRoutePage,
     siteThemeType: siteThemeType,
     showRouteTable: showRouteTable
 };
