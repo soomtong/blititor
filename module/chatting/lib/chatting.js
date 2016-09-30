0c6b19c9e934afcc98c79c130f75684dcad20454
upgle <admin@upgle.com>
2016-09-30 20:12:33 +0900

    chatting module nickname 변수 선언 부분 리팩토링.

--- module/chatting/lib/chatting.js
+++ module/chatting/lib/chatting.js
@@ -20,9 +20,11 @@ function socketWrapper(io, callback){
 
     io.sockets.on('connection', function(socket){
         winston.verbose('a user connected');
-        var session = socket.request.session, nickname;
+
+        var nickname;
+        var session = socket.request.session;
         var uuid = session.passport ? session.passport.user : null;
-        console.log(uuid);
+
         if(uuid){
             findAccountByUUID(uuid, function(err, data){
                 nickname = data.nickname;
