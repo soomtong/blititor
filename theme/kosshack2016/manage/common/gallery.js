789862e78f362b9eb92bc0132e8f1ee19b5babe9
soomtong <soomtong@gmail.com>
2016-10-07 13:39:03 +0900

    fixed another gallery uploader

--- theme/kosshack2016/manage/common/gallery.js
+++ theme/kosshack2016/manage/common/gallery.js
@@ -1,6 +1,7 @@
 /* global $, window */
 'use strict';
 var thumbnail = '/upload/gallery/thumb/';
+var categoryID = 1;
 
 $(function () {
     var done = function (e, data) {
@@ -8,9 +9,9 @@ $(function () {
 
         var path = file.path.split('/');
         var name = path[path.length - 1];
-        var categoryID = $('#add_image').parent().parent().parent().data('id');
+        // categoryID = $(e.target).parent().parent().parent().parent().parent().data('id');
 
-        console.log(file);
+        console.log(categoryID, file);
 
         if (file.errors && file.errors.length > 0) {
             // $('#files').empty();
@@ -18,15 +19,26 @@ $(function () {
         } else {
             // $('#files').empty();
 
-            $('<img class="thumbnail" />').attr('src', thumbnail + name).appendTo('#files');
+            $('<img class="thumbnail" />').attr('src', thumbnail + name).appendTo('#holder_' + categoryID + ' .img-holder .files');
             $('<input name="thumb" type="hidden">').attr('value', name).appendTo('#add_image');
             $('<input name="image" type="hidden">').attr('value', file.originalname).appendTo('#add_image');
             $('<input name="path" type="hidden">').attr('value', file.path).appendTo('#add_image');
             $('<input name="category" type="hidden">').attr('value', categoryID).appendTo('#add_image');
 
-            $('#add_image button[type=submit]').prop('disabled', false).text('전송하기').addClass('button-secondary').removeClass('button-warning');
+            $('#add_image button[type=submit]').prop('disabled', false).text('전송하기').addClass('button-error').removeClass('button-warning');
 
             $('#gallery_image_file').prop('disabled', true).parent().addClass('disabled');
+
+/*
+            $.get('/gallery/image/' + Number(categoryID), {}, function (response, status, xhr) {
+                response.map(function (item) {
+                    $('<img class="thumbnail" />')
+                        .attr('src', thumbnail + item['thumbnail'])
+                        .attr('title', item['image'])
+                        .appendTo('#holder_' + categoryID + ' .img-holder .files');
+                });
+            });
+*/
         }
     };
 
@@ -39,8 +51,6 @@ $(function () {
 
     var token = $('input[name="_csrf"]').val();
 
-    // $('#progress .progress-bar').css('width', '50%');
-
     $('#gallery_image_file').fileupload({
         url: url,
         headers: { 'X-CSRF-Token': token },
@@ -49,14 +59,29 @@ $(function () {
         done: done,
         progressall: progress
     });
-});
 
-$.get('/gallery/image/1', {}, function (response, status, xhr) {
-    response.map(function (item) {
-        $('<img class="thumbnail" />')
-            .attr('src', thumbnail + item['thumbnail'])
-            .attr('title', item['image'])
-            .appendTo('#files');
+    $('.gallery-wrap').map(function (idx, el) {
+        var cate_id = $(el).data('id');
+        if (cate_id) {
+            $.get('/gallery/image/' + Number($(el).data('id')), {}, function (response, status, xhr) {
+                response.map(function (item) {
+                    // console.log(item);
+                    $('<img class="thumbnail" />')
+                        .attr('src', thumbnail + item['thumbnail'])
+                        .attr('title', item['image'])
+                        .appendTo('#holder_' + cate_id + ' .img-holder .files');
+                });
+                $('#cate_' + cate_id + ' .counter').text(response.length);
+            });
+        }
+    });
+
+    $('.gallery').on('click', 'tr.toggle-category', function (e) {
+        categoryID = $(this).data('id');
+
+        $('tr.toggle-category').removeClass('selected-category');
+        $(this).addClass('selected-category');
+
+        $('#add_image').show();
     });
 });
-
