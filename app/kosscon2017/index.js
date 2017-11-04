28de22fe5205aca18cc2d5209cd1d11541757d65
Aiden <soomtong@gmail.com>
2017-11-05 00:48:40 +0900

    Added kosscon 2017

---
+++ app/kosscon2017/index.js
@@ -0,0 +1,65 @@
+// load packages
+var express = require('express');
+var winston = require('winston');
+
+// load cores
+var misc = require('../../core/lib/misc');
+
+// load modules
+var Account = require('../../module/account');
+var Site = require('../../module/site');
+var Admin = require('../../module/administrator');
+var Manager = require('../../module/manager');
+var Counter = require('../../module/counter');
+var Reservation = require('../../module/reservation');
+var Gallery = require('../../module/gallery');
+
+// load locals
+var app = require('./app.json');
+var menu = require('./menu');
+
+// init
+var router = express.Router();
+var routeTable = misc.getRouteTable();
+var appLocals = Site.exposeAppLocals(app.locals, menu);
+
+// middleware
+router.use(Account.middleware.exposeLocals);
+router.use(Site.middleware.exposeLocals);
+router.use(Admin.middleware.exposeMenu);
+router.use(Manager.middleware.exposeMenu);
+
+// route
+router.use(Admin.route);       // to manage accounts
+router.use(Manager.route);     // to view log module
+
+// it uses common feature for each admin and manager, then assign in app router.
+// other features use each module's router. eg, modifying account records or log records
+router.get(routeTable.account_root + routeTable.account.signOut, Account.signOut);
+
+// no need to count
+
+// need to place down here for except admin page log
+router.use(Counter.middleware.sessionCounter);
+router.use(Counter.middleware.pageCounter);
+
+// modules
+router.use(routeTable.gallery_root, Gallery.route);
+router.use(routeTable.reservation_root, Reservation.route);
+/*
+router.post('/register', function (req, res) {
+    console.log(req.body);
+
+    res.redirect('back');
+});
+*/
+
+// bind static page
+router.all('/2016', Site.redirect(routeTable.root));
+
+Site.bindMenu(menu, router);
+
+module.exports = {
+    exposeLocals: appLocals,
+    router: router,
+};
---
+++ app/kosscon2017/menu.js
@@ -0,0 +1,142 @@
+var fs = require('fs');
+var winston = require('winston');
+
+var misc = require('../../core/lib/misc');
+
+var routeTable = misc.getRouteTable();
+
+var Menu = [
+    {
+        id: 'index',
+        name: 'KossLab Conference 2017',
+        url: routeTable.root
+    },
+    {
+        id: '2017home_preview',
+        name: 'Home Preview',
+        url: '/preview'
+    },
+    {
+        id: 'conference_gallery',
+        name: '갤러리',
+        url: '/gallery'
+    },
+    {
+        id: 'conference_program',
+        name: 'Program',
+        url: '/program'
+    },
+    {
+        id: 'conference_keynote',
+        name: 'Keynote',
+        useSubPath: true,
+        url: '/program/keynote'
+    },
+    {
+        id: 'conference_presentation',
+        name: 'Presentation',
+        useSubPath: true,
+        url: '/program/presentation'
+    },
+    {
+        id: 'conference_tutorial',
+        name: 'Tutorial',
+        useSubPath: true,
+        url: '/program/tutorial'
+    },
+    {
+        id: 'register_form',
+        name: 'Reservation Form',
+        url: '/register'
+    },
+    {
+        id: 'register_confirm',
+        name: 'Reservation Confirm',
+        useSubPath: true,
+        url: '/register/confirm'
+    },
+    {
+        id: 'edm_1',
+        name: 'Direct Mail 1',
+        useSubPath: true,
+        url: '/register/dm1'
+    },
+    {
+        id: '2015index',
+        name: 'KOSSCON 2015',
+        url: '/2015'
+    },
+    {
+        id: '2014index',
+        name: 'KOSSCON 2014',
+        url: '/2014'
+    },
+    {
+        id: '2014program',
+        name: 'KOSSCON 2014',
+        useSubPath: true,
+        url: '/2014/program'
+    },
+    {
+        id: '2014info',
+        name: 'KOSSCON 2014',
+        useSubPath: true,
+        url: '/2014/info'
+    },
+    {
+        id: '2014registration',
+        name: 'KOSSCON 2014',
+        useSubPath: true,
+        url: '/2014/registration'
+    }
+];
+
+var AdminMenu = [
+    {
+        id: 'index',
+        name: '관리자 홈',
+        url: routeTable.admin_root
+    },
+    {
+        id: 'new',
+        name: '신규 계정 생성',
+        url: routeTable.admin_root + routeTable.admin.accountNew
+    },
+    {
+        id: 'manage',
+        name: '운영',
+        url: routeTable.manage_root
+    }
+];
+
+var ManagerMenu = [
+    {
+        id: 'index',
+        name: '운영자 홈',
+        url: routeTable.manage_root
+    },
+    {
+        id: 'account',
+        name: '계정',
+        url: routeTable.manage_root + routeTable.manage.account
+    },
+    {
+        id: 'reservation',
+        name: '예약자 명단',
+        url: routeTable.manage_root + routeTable.manage.reservation
+    },
+    {
+        id: 'tutorial',
+        name: '튜토리얼 현황',
+        url: routeTable.manage_root + routeTable.manage.tutorial
+    },
+    {
+        id: 'gallery',
+        name: '갤러리',
+        url: routeTable.manage_root + routeTable.manage.gallery
+    },
+];
+
+module.exports = Menu;
+module.exports.AdminMenu = AdminMenu;
+module.exports.ManagerMenu = ManagerMenu;
